e6373464bfaeaad35b7ecf5f7dbf8a597ac8a8cd
xuxinwszsw <2297725961@qq.com>
2025-09-06 23:25:49 +0800

    1

--- src/test.java
+++ src/test.java
@@ -1,5 +1,9 @@
 public class test {
     public static void main(String[] args) {
+        System.out.println( "hello world");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
 
     }
 }
